032f8c87ba0cb4a3dd412b9d70abdd5f512ff2c5
svarz <svarz@terpmail.umd.edu>
2022-02-19 17:46:35 -0500

    Tetris C5 added divs to index bc im dumb :)

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -48,15 +48,29 @@ document.addEventListener('DOMContentLoaded', () => {~
     let random = Math.floor(Math.random()*theTetrominoes.length)
     let current = theTetrominoes[random][0]
 
-    //draw the first roatino in the first tetromino
+    //draw the Tetromino
     function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino')
         })
     }
 
-    draw()
+    //undraw the Tetromino
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
     
+    //make the tetromino move down every second
+    timerId = setInterval(moveDown, 1000)
+
+    //move down function
+    function moveDon() {
+        undraw()
+        currentPosition += width
+        draw()
+    }
 
 
 })
