6d79cb6b8564089c3b715d7c0cf2922b4e19f8c7
svarz <svarz@terpmail.umd.edu>
2022-03-15 20:39:50 -0400

    Quick fix

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -2,40 +2,40 @@ let slidePosition = 0;
 const slides = document.getElementsByClassName('carousel_item');
 const totalSlides = slides.length;
 
-document.getElementById('carousel_button--next').addEventListener("click", function() {
+document
+  .getElementById('carousel_button--next')
+  .addEventListener('click', moveToNextSlide() {
     moveToNextSlide();
-  })
+  });
 
-document.getElementById('carousel_button--prev').addEventListener("click", function() {
+document
+  .getElementById('carousel_button--prev')
+  .addEventListener('click', moveToPrevSlide() {
     moveToPrevSlide();
-})
-
-
+  });
 
 function updateSlidePosition() {
-    for (let slide of slides) {
-        slide.classList.remove('carousel_item--visible');
-        slide.classList.add('carousel_item--hidden');
-    }
-    slides[slidePosition].classList.add('carousel_item--visible')
+  for (let slide of slides) {
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
+  }
+  slides[slidePosition].classList.add('carousel_items--visible');
 }
 
 function moveToNextSlide() {
-
-    if (slidePosition === (totalSlides - 1) {
-        slidePosition = 0;
-    } else {
-        slidePosition++;
-    }
-    updateSlidePosition();
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
+  updateSlidePosition();
 }
 
 function moveToPrevSlide() {
-
-    if (slidePosition === 0) {
-        slidePosition = 0;
-    } else {
-        slidePosition--;
-    }
-    updateSlidePosition();
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition -= 1;
+  }
+  updateSlidePosition();
 }
